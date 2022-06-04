5b5a372537c0001de2aa0c693d48fbf78d753b1b
diego-bm <93934897+diego-bm@users.noreply.github.com>
2022-06-04 12:47:53 -0300

    Primeiro commit

---
+++ admso.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+menu(){
+    echo " ______________________________________________________ "
+    echo "|1- Contar arquivos da pasta                           |"
+    echo "|2- Tornar arquivo executável                          |"
+    echo "|3- Copiar arquivo                                     |"
+    echo "|4- Tirar permissão de \"outros\"                        |"
+    echo "|5- Dar permissão para \"grupo\" de R.W. (Read e Write)  |"
+    echo "|0- Sair                                               |"
+    echo "|______________________________________________________|"
+
+    read opcao
+    case $opcao in
+        0) exit ;;
+        1) contarArquivos ;;
+        2) tornarX ;;
+        3) copiarArquivo ;;
+        4) tirarPermissoesOutros ;;
+        5) darPermissoesRWGrupo ;;
+        *) ;;
+    esac
+}
+
+contarArquivos(){
+        echo "Indique o caminho da pasta a se contar os arquivos presentes:"
+        read caminho
+        arquivos=`ls -l $caminho | wc -l`
+        ((arquivos=$arquivos-1))
+        echo "Existe(m) $arquivos arquivo(s) no caminho $caminho"
+        menu
+    }
+
+    tornarX(){
+        echo "Hi"
+    }
+
+    copiarArquivo(){
+        echo "Hi"
+    }
+
+    tirarPermissoesOutros(){
+        echo "Hi"
+    }
+
+    darPermissoesRWGrupo(){
+        echo "Hi"
+    }
+
+menu
