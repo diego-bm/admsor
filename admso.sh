e527ee764fd0f630f47cfa1cc3f6e8fe4612341f
diego-bm <dborgesmoraes@outlook.com>
2022-07-10 18:06:18 -0300

    Adicionada funções do Samba e implementação do mesmo na criação de um usuário

--- admso.sh
+++ admso.sh
@@ -2,8 +2,11 @@
 #TODO: FUNCIONALIZAR REPETÊNCIAS
 #TODO: USAR /DEV/NULL PARA DEIXAR O PROGRAMA MAIS USER-FRIENDLY
 #TODO: ADICIONAR MAIS VALIDAÇÕES NA FUNÇÃO 13
+#TODO: SEPARAR PROGRAMA EM PROGRAMAS MENORES (SUB-PROGRAMAS)
 
-menu(){
+Menu(){
+    clear
+
     echo " ______________________________________________________ "
     echo "|1- Contar arquivos da pasta                           |"
     echo "|2- Tornar arquivo executável                          |"
@@ -17,31 +20,39 @@ menu(){
     echo "|10- Criar um grupo                                    |"
     echo "|11- Apagar um grupo                                   |"
     echo "|12- Mostrar meu IP                                    |"
-    echo "|13- Compartilhar uma pasta em rede através do Samba   |"
+    echo "|13- [SAMBA] Compartilhar uma pasta em rede            |"
+    echo "|14- [SAMBA] Mostrar compartilhamentos ativos          |"
     echo "|0- Sair                                               |"
     echo "|______________________________________________________|"
 
     read opcao
     case $opcao in
         0) exit ;;
-        1) contarArquivos ;;
-        2) tornarX ;;
-        3) copiarArquivo ;;
-        4) tirarPermissoesOutros ;;
-        5) darPermissoesRWGrupo ;;
-        6) listarArquivos ;;
-        7) renomear ;;
-        8) criarUsuario ;;
-        9) apagarUsuario ;;
-        10) criarGrupo ;;
-        11) apagarGrupo ;;
-        12) mostrarIP ;;
-        13) compartilharSamba ;;
+        1) ContarArquivos ;;
+        2) TornarX ;;
+        3) CopiarArquivo ;;
+        4) TirarPermissoesOutros ;;
+        5) DarPermissoesRWGrupo ;;
+        6) ListarArquivos ;;
+        7) Renomear ;;
+        8) CriarUsuario ;;
+        9) ApagarUsuario ;;
+        10) CriarGrupo ;;
+        11) ApagarGrupo ;;
+        12) MostrarIP ;;
+        13) CompartilharSamba ;;
+        14) ListarCompartilhamentosSamba ;;
         *) echo "Opção inválida." ;;
     esac
 }
 
-contarArquivos(){
+AperteEnter(){
+    read -p "Aperte ENTER para continuar..."
+
+    Menu
+}
+
+ContarArquivos(){
     echo "Indique o caminho da pasta a se contar os arquivos presentes:"
     read caminho
     arquivos=`ls -l $caminho | wc -l`
@@ -57,10 +68,10 @@ contarArquivos(){
         echo "Não há nenhum arquivo no caminho especificado."
     fi
 
-    menu
+    AperteEnter
 }
 
-tornarX(){
+TornarX(){
     echo "Indique o caminho do script a tornar executável (todos terão permissão para executar):"
     read caminho
 
@@ -73,43 +84,43 @@ tornarX(){
             echo "ERRO: o arquivo não teve a permissão concedida."
             ls -lap $caminho
             echo "Tente novamente."
-            tornarX
+            TornarX
         fi
     else
         echo "O arquivo ou diretório não existe. Tente novamente."
-        tornarX
+        TornarX
     fi    
 
-    menu
+    AperteEnter
 }
 
-copiarArquivo(){
-    copiarArquivoCheck1         
+CopiarArquivo(){
+    CopiarArquivoCheck1         
 
     if [ -e $caminhoCopia ]; then 
         echo "Copiado com sucesso!"
         ls -lap $caminhoCopia
     else
         echo "ERRO: o arquivo não foi copiado. Verifique o erro e tente novamente."
-        copiarArquivo
+        CopiarArquivo
     fi
 
-    menu
+    AperteEnter
 }
 
-copiarArquivoCheck1(){
+CopiarArquivoCheck1(){
     echo "Insira o caminho do arquivo a ser copiado:"
     read caminho
 
     if [ -e $caminho ]; then
-        copiarArquivoCheck2
+        CopiarArquivoCheck2
     else
         echo "O arquivo não existe, tente novamente."
-        copiarArquivoCheck1
+        CopiarArquivoCheck1
     fi
 }
 
-copiarArquivoCheck2(){
+CopiarArquivoCheck2(){
     echo "Agora, insira o diretório para onde a cópia deve ser feita:"
     read caminhoCopia
 
@@ -117,11 +128,11 @@ copiarArquivoCheck2(){
         cp $caminho $caminhoCopia
     else
         echo "Insira um diretório válido"
-        copiarArquivoCheck2
+        CopiarArquivoCheck2
     fi 
 }
 
-tirarPermissoesOutros(){
+TirarPermissoesOutros(){
     echo "O arquivo do qual você inserir o caminho a seguir não terá permissão nenhuma dada a outros:"
     read caminho
 
@@ -134,17 +145,17 @@ tirarPermissoesOutros(){
         else
             echo "ERRO: a permissão não foi alterada."
             ls -lap $caminho
-            tirarPermissoesOutros
+            TirarPermissoesOutros
         fi
     else
         echo "O arquivo não existe. Tente novamente."
-        tirarPermissoesOutros
+        TirarPermissoesOutros
     fi
 
-    menu
+    AperteEnter
 }
 
-darPermissoesRWGrupo(){
+DarPermissoesRWGrupo(){
     echo "O arquivo do qual você inserir o caminho a seguir terá permissões de leitura e escrita pelo grupo:"
     read caminho
     if [ -e $caminho ]; then
@@ -158,146 +169,168 @@ darPermissoesRWGrupo(){
         else
             echo "ERRO: a permissão não foi alterada."
             ls -lap $caminho
-            darPermissoesRWGrupo
+            DarPermissoesRWGrupo
         fi
     else
         echo "O arquivo não existe. Tente novamente."
-        darPermissoesRWGrupo
+        DarPermissoesRWGrupo
     fi
 
-    menu
+    AperteEnter
 }
 
-listar(){
+Listar(){
     echo "Listando..."
     ls -lap $caminho
 }
 
-listarArquivos(){
+ListarArquivos(){
     echo "Insira o caminho a ser listado:"
     read caminho
     if [ -d $caminho ]; then
-        listar
+        Listar
     else
         echo "Não é um diretório válido ou existente. Tente novamente."
-        listarArquivos
+        ListarArquivos
     fi
 }
 
-renomear(){
+Renomear(){
     echo "Insira o caminho do DIRETÓRIO onde está o arquivo:"
     read caminho
 
     if [ -d $caminho ]; then
-        renomearCheck1
+        RenomearCheck1
     else
         echo "ERRO: o caminho não especifica um diretório. Tente novamente."
-        renomear
+        Renomear
     fi
 }
 
-renomearCheck1(){
+RenomearCheck1(){
     echo "Insira o nome do arquivo:"
     read arquivo
     arquivosPresentes=$(ls $caminho)
 
-    if [[ arquivosPresentes == *"$arquivo"* ]]; then
-        renomearCheck2
+    if [[ "$arquivosPresentes" == *"$arquivo"* ]]; then
+        RenomearCheck2
     else
         echo "ERRO: o arquivo especificado não existe nesse diretório."
     fi
 }
 
-renomearCheck2(){
+RenomearCheck2(){
     echo "Insira o novo nome desse arquivo:"
     read novoNome
     if [[ arquivosPresentes == *"$novoNome"* ]]; then
-        renomearCheck3
+        RenomearCheck3
     else
         mv $caminho/$arquivo $caminho/$novoNome
     fi
 }
 
-renomearCheck3(){
+RenomearCheck3(){
     echo "Você vai substituir um arquivo com o mesmo nome por esse que está renomeando. Deseja continuar? (s/n)"
-    renomearCheck4
+    RenomearCheck4
 }
 
-renomearCheck4(){
+RenomearCheck4(){
     read opcao
     case $opcao in
         "s") 
             mv $caminho/$arquivo $caminho/$novoNome ;;
         "n") 
             echo "Então..."
-            renomearCheck2 ;;
+            RenomearCheck2 ;;
         *) 
             echo "Opção inválida. Responda de novo." ;
-            renomearCheck4 ;;
+            RenomearCheck4 ;;
     esac
 }
 
-criarUsuario(){
+CriarUsuario(){
     echo "Insira o nome do usuário a ser criado, o Linux cuidará do resto para você (em inglês):"
     read nomeUsuario
     if [[ nomeUsuario == "" ]]; then
         echo "ERRO: insira um nome para o usuário."
-        criarUsuario
+        CriarUsuario
     else
         sudo adduser $nomeUsuario
+        CriarUsuarioCheckSamba
     fi
 }
 
-apagarUsuario(){
-    listarUsuarios
+CriarUsuarioCheckSamba(){
+    read -p "Deseja criar um usuário do Samba para esse novo usuário? (S/n) " opcao
+    case $opcao in
+        "s") 
+            echo "Criando usuário do Samba..." ;
+            CriarUsuarioSamba ;;
+        "S") 
+            echo "Criando usuário do Samba..." ;
+            CriarUsuarioSamba ;;
+        "n") 
+            echo "Ok, retornando ao menu..." ;
+            AperteEnter ;;
+        "N") 
+            echo "Ok, retornando ao menu..." ;
+            AperteEnter ;;
+        *) 
+            echo "Opção inválida. Responda de novo." ;
+            CriarUsuarioCheckSamba ;;
+    esac
+}
+
+ApagarUsuario(){
+    ListarUsuarios
     echo ""
     echo "Recomendamos apenas deletar usuários que vêm abaixo do nome do seu."
     echo "Escreva abaixo o nome do usuário a ser deletado:"
     read usuario
 
     sudo userdel $usuario
-    listarUsuarios
+    ListarUsuarios
 }
 
-criarGrupo(){
+CriarGrupo(){
     echo "Qual o nome do grupo que quer criar?"
     read grupo
     sudo groupadd $grupo
-    listarGrupos
+    ListarGrupos
 }
 
-apagarGrupo(){
-    listarGrupos
+ApagarGrupo(){
+    ListarGrupos
 
     echo "Insira o nome do grupo a ser deletado. "
     echo "Recomendamos apenas deletar grupos que vêm abaixo do nome do seu."
     read grupo
     sudo groupdel $grupo
 
-    listarGrupos
+    ListarGrupos
 }
 
-mostrarIP(){
+MostrarIP(){
     echo "Trabalhando..."
     ip=`curl ifconfig.me`
     echo "Seu ip é $ip"
 }
 
-listarUsuarios(){
+ListarUsuarios(){
     echo "Os usuários dessa maquina são:"
     echo "____________________________"
     cut -d: -f1 /etc/passwd
     echo "____________________________"
 }
 
-listarGrupos(){
+ListarGrupos(){
     echo "Os grupos dessa maquina são:"
     echo "____________________________"
     cat /etc/group | cut -d: -f1
     echo "____________________________"
 }
 
-compartilharSamba(){
+CompartilharSamba(){
     echo "Qual o nome do compartilhamento?"
     read compartilhamento
 
@@ -355,7 +388,40 @@ compartilharSamba(){
     systemctl restart smbd
 
     echo "Terminado!"
-    menu
+    AperteEnter
 }
 
-menu
+CriarUsuarioSamba(){
+    sudo smbpasswd -a $nomeUsuario
+    AperteEnter
+}
+
+ListarCompartilhamentosSamba(){
+    echo "Você precisa logar com um usuário do Samba para conseguir ver as conexões ativas."
+    read -p "Digite o usuário: " usuario
+
+    usuarios=`cut -d: -f1 /etc/passwd`
+
+    if [[ "$usuarios" == *"$usuario"* ]]; then
+        smbclient -L 127.0.1.1 -U $usuario
+    else
+        read -p "O usuário \"$usuario\" não existe. Deseja criá-lo? (S/n) " opcao
+        case $opcao in
+            "s") 
+                echo "Criando novo usuario..." ;
+                CriarUsuario ;;
+            "S") 
+                echo "Criando novo usuario..." ;
+                CriarUsuario ;;
+            "n") 
+                ListarCompartilhamentosSamba ;;
+            "N") 
+                ListarCompartilhamentosSamba ;;
+            *) 
+                echo "Opção inválida. Responda de novo." ;
+                ListarCompartilhamentosSamba ;;
+        esac
+    fi
+}
+
+Menu
